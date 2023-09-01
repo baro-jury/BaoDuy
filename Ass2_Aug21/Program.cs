520d00acc4edb1d74aa9746475bc4a7df8f40982
BaoDuy <duypl572002@gmail.com>
2023-09-01 17:55:42 +0700

    Update assignment 21/08/2023

--- Ass2_Aug21/Program.cs
+++ Ass2_Aug21/Program.cs
@@ -13,18 +13,38 @@
     {
         try
         {
-            Console.WriteLine("Enter constants for an equation as 'a * x + b = 0'");
+            Console.WriteLine("Enter constants for an equation as 'a*x + b = 0'");
             Console.Write("a = ");
             double a = Convert.ToDouble(Console.ReadLine());
             Console.Write("b = ");
             double b = Convert.ToDouble(Console.ReadLine());
             if (a != 0)
             {
-                Console.WriteLine("The solution for '{0} * x + {1} = 0' is {2}", a, b, -b / a);
+                if (b == 0) Console.WriteLine("The solution for '{0}*x = 0' is {1}", a, 0);
+                else Console.WriteLine("The solution for '{0}*x + {1} = 0' is {2}", a, b, -b / a);
             }
             else
             {
                 Console.WriteLine(b == 0 ? "The solution is all x!" : "No solution!");
+            }
+
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Enter constants for an equation as 'a*x + b = c'");
+            Console.Write("a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c = ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            if (a != 0)
+            {
+                if (b == 0) Console.WriteLine("The solution for '{0}*x = {1}' is {2}", a, c, c / a);
+                else Console.WriteLine("The solution for '{0}*x + {1} = {2}' is {3}", a, b, c, (c - b) / a);
+            }
+            else
+            {
+                Console.WriteLine(b == c ? "The solution is all x!" : "No solution!");
             }
         }
         catch (Exception ex)
