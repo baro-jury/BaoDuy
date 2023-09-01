a7b75e4864c5f075964bb86dbda146f4b2c6503b
BaoDuy <duypl572002@gmail.com>
2023-09-01 22:37:33 +0700

    Update assignment 24/08/2023

--- Ass3_Aug24/Program.cs
+++ Ass3_Aug24/Program.cs
@@ -2,6 +2,67 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DesignMenu();
+        Console.WriteLine("-------------------------------");
+        //ResolveQuadraticEquation();
+        Console.WriteLine("-------------------------------");
+        //CalculateBodyMassIndex();
+        Console.WriteLine("-------------------------------");
+        //ReadNumbersIntoWords();
+    }
+
+    public static void DesignMenu()
+    {
+        while(true)
+        {
+            int choice;
+            Console.WriteLine("====== Menu ======");
+            Console.WriteLine("1. Draw the triangle");
+            Console.WriteLine("2. Draw the square");
+            Console.WriteLine("3. Draw the rectangle");
+            Console.WriteLine("0. Exit");
+            Console.Write("\n=> Enter your choice: ");
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine();
+                    Console.WriteLine("Draw the triangle");
+                    Console.WriteLine("******");
+                    Console.WriteLine("*****");
+                    Console.WriteLine("****");
+                    Console.WriteLine("***");
+                    Console.WriteLine("**");
+                    Console.WriteLine("*");
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    Console.WriteLine();
+                    Console.WriteLine("Draw the square");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    Console.WriteLine("Draw the rectangle");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine("* * * * * *");
+                    Console.WriteLine();
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("No choice!");
+                    Console.WriteLine();
+                    break;
+            }
+        }
     }
 }
