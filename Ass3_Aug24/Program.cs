91ccf3cef9b3736aba496de732a7d3417221f7dd
BaoDuy <duypl572002@gmail.com>
2023-09-06 12:11:15 +0700

    Update assignment 24/08/2023

--- Ass3_Aug24/Program.cs
+++ Ass3_Aug24/Program.cs
@@ -11,7 +11,8 @@
 
     public static void DesignMenu()
     {
-        while (true)
+        bool isRunning = true;
+        while (isRunning)
         {
             int choice;
             Console.WriteLine("====== Menu ======");
@@ -54,7 +55,7 @@
                     Console.WriteLine();
                     break;
                 case 0:
-                    Environment.Exit(0);
+                    isRunning = false;
                     break;
                 default:
                     Console.WriteLine("No choice!");
@@ -68,7 +69,7 @@
     {
         int number;
         Console.Write("Enter a number: ");
-        number = int.Parse(Console.ReadLine());
+        number = GetInteger(Int32.MinValue, Int32.MaxValue, "Enter again!");
         if (number < 2) Console.WriteLine(number + " is not a prime");
         else
         {
@@ -90,6 +91,171 @@
 
     public static void ShowGeometry()
     {
-        
+        bool isRunning = true;
+        string resultInLine = "";
+        while (isRunning)
+        {
+            int choice;
+            Console.WriteLine("====== Menu ======");
+            Console.WriteLine("1. Print the rectangle");
+            Console.WriteLine("2. Print the square triangle");
+            Console.WriteLine("3. Print isosceles triangle");
+            Console.WriteLine("0. Exit");
+            Console.Write("\n=> Enter your choice: ");
+            choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("\nPrint the rectangle");
+                    Console.Write("Length = ");
+                    int length = GetInteger(2, Int32.MaxValue, "Enter again!");
+                    Console.Write("Width = ");
+                    int width = GetInteger(1, length - 1, "Enter again!");
+                    Console.WriteLine();
+                    for (int i = 1; i <= width; i++)
+                    {
+                        for (int j = 1; j <= length; j++)
+                        {
+                            Console.Write("* ");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    Console.WriteLine("\nPrint the square triangle");
+                    Console.Write(
+                        "------------------------------------\n" +
+                        "| 1 - Bottom-left square triangle  |\n" +
+                        "| 2 - Top-left square triangle     |\n" +
+                        "| 3 - Bottom-right square triangle |\n" +
+                        "| 4 - Top-right square triangle    |\n" +
+                        "------------------------------------\n" +
+                        "Type of square triangle = ");
+                    int type = GetInteger(1, 4, "Enter again!");
+                    Console.Write("Height = ");
+                    int height = GetInteger(1, Int32.MaxValue, "Enter again!");
+                    Console.WriteLine();
+                    switch (type)
+                    {
+                        case 1: //bottom left
+                            for (int i = 1; i <= height; i++)
+                            {
+                                for (int j = 1; j <= i; j++)
+                                {
+                                    Console.Write("* ");
+                                }
+                                Console.WriteLine();
+                            }
+                            Console.WriteLine();
+                            break;
+                        case 2: //top left
+                            for (int i = height; i >= 1; i--)
+                            {
+                                for (int j = 1; j <= i; j++)
+                                {
+                                    Console.Write("* ");
+                                }
+                                Console.WriteLine();
+                            }
+                            Console.WriteLine();
+                            break;
+                        case 3: //bottom right
+                            for (int i = 1; i <= height; i++)
+                            {
+                                resultInLine = "";
+                                for (int j = 1; j <= i; j++)
+                                {
+                                    resultInLine += "* ";
+                                    resultInLine.Trim();
+                                }
+                                for (int j = 1; j <= (height - i); j++)
+                                {
+                                    resultInLine = "  " + resultInLine;
+                                }
+                                Console.WriteLine(resultInLine);
+                            }
+                            Console.WriteLine();
+                            break;
+                        case 4: //top right
+                            for (int i = height; i >= 1; i--)
+                            {
+                                resultInLine = "";
+                                for (int j = 1; j <= i; j++)
+                                {
+                                    resultInLine += "* ";
+                                    resultInLine.Trim();
+                                }
+                                for (int j = 1; j <= (height - i); j++)
+                                {
+                                    resultInLine = "  " + resultInLine;
+                                }
+                                Console.WriteLine(resultInLine);
+                            }
+                            Console.WriteLine();
+                            break;
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("\nPrint isosceles triangle");
+                    Console.Write("Height = ");
+                    height = GetInteger(1, Int32.MaxValue, "Enter again!");
+                    Console.WriteLine();
+                    for (int i = 1; i <= height; i++)
+                    //for (int i = height; i >= 1; i--)
+                    {
+                        resultInLine = "";
+                        for (int j = 1; j <= i; j++)
+                        {
+                            resultInLine += "* ";
+                            resultInLine.Trim();
+                        }
+                        for (int j = 1; j <= (height - i); j++)
+                        {
+                            resultInLine += " ";
+                            resultInLine = " " + resultInLine;
+                        }
+                        Console.WriteLine(resultInLine);
+                    }
+                    Console.WriteLine();
+                    break;
+                case 0:
+                    //Environment.Exit(0);
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("No choice!");
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
+
+    private static int GetInteger(int min, int max, string errorMsg)
+    {
+        while (true)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < min || number > max)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else return number;
+            }
+            catch (ArgumentOutOfRangeException aoore)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Out of range!");
+                Console.ResetColor();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(errorMsg);
+                Console.ResetColor();
+            }
+        }
     }
 }
