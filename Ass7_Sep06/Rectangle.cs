054ff6d0a1e7a2ea0a5be45147fcb73d949e56a4
BaoDuy <duypl572002@gmail.com>
2023-09-07 23:07:33 +0700

    Update assignment 06/09/2023

---
+++ Ass7_Sep06/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass7_Sep06
+{
+    public class Rectangle
+    {
+    }
+}
