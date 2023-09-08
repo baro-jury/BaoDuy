d8b5efd45817e0f1cd43041b2b0286fcd65eae1e
BaoDuy <duypl572002@gmail.com>
2023-09-08 23:56:27 +0700

    Update assignment 08/09/2023

--- Ass8_Sep08/Program.cs
+++ Ass8_Sep08/Program.cs
@@ -1,7 +1,34 @@
-﻿class Program
+﻿using Ass8_Sep08;
+using System;
+
+class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Shape shape = new Shape();
+        Console.WriteLine(shape);
+        shape = new Shape("red", false);
+        Console.WriteLine(shape);
+        Console.WriteLine("-------------------------------");
+        Circle circle = new Circle();
+        Console.WriteLine(circle);
+        circle = new Circle(3.5);
+        Console.WriteLine(circle);
+        circle = new Circle(3.5, "indigo", false);
+        Console.WriteLine(circle);
+        Console.WriteLine("-------------------------------");
+        Rectangle rectangle = new Rectangle();
+        Console.WriteLine(rectangle);
+        rectangle = new Rectangle(2.3, 5.8);
+        Console.WriteLine(rectangle);
+        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        Console.WriteLine(rectangle);
+        Console.WriteLine("-------------------------------");
+        Square square = new Square();
+        Console.WriteLine(square);
+        square = new Square(2.3);
+        Console.WriteLine(square);
+        square = new Square(5.8, "yellow", true);
+        Console.WriteLine(square);
     }
 }
