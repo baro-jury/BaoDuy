dc44b36b09a9231fd960078e68c85ecac8ddd009
BaoDuy <duypl572002@gmail.com>
2023-09-08 18:55:41 +0700

    Update assignment 06/09/2023

--- Ass7_Sep06/Program.cs
+++ Ass7_Sep06/Program.cs
@@ -1,7 +1,28 @@
-﻿class Program
+﻿using Ass7_Sep06;
+
+class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CreateRectangle();
+        Console.WriteLine("-------------------------------");
+        //FindMinValueInArray();
+        Console.WriteLine("-------------------------------");
+        //RemoveElementFromArray();
+        Console.WriteLine("-------------------------------");
+        //CountOccurrencesOfCharacter();
+    }
+
+    public static void CreateRectangle()
+    {
+        Console.Write("Enter the width: ");
+        double width = double.Parse(Console.ReadLine());
+        Console.Write("Enter the height: ");
+        double height = double.Parse(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(width, height);
+        Console.WriteLine("Your rectangle: " + rectangle.Display());
+        Console.WriteLine("Perimeter of the Rectangle: " + rectangle.GetPerimeter());
+        Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
     }
 }
