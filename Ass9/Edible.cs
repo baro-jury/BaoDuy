2b7232e028110c5a97a674bc1dc0848666f500eb
BaoDuy <duypl572002@gmail.com>
2023-09-09 19:36:28 +0700

    Update ass9

---
+++ Ass9/Edible.cs
@@ -0,0 +1,28 @@
+﻿namespace Ass9
+{
+    public interface Edible
+    {
+        public string HowToEat();
+    }
+
+    public abstract class Fruit : Edible
+    {
+        public abstract string HowToEat();
+    }
+
+    public class Apple : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Apple could be slided";
+        }
+    }
+
+    public class Orange : Fruit
+    {
+        public override string HowToEat()
+        {
+            return "Orange could be juiced";
+        }
+    }
+}
---
+++ Ass9/Program.cs
@@ -0,0 +1,40 @@
+﻿using Ass9;
+using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        CreateAnimals();
+        Console.WriteLine("-------------------------------");
+        //CreateCircle();
+        Console.WriteLine("-------------------------------");
+        //CreatePoint2D();
+    }
+
+    public static void CreateAnimals()
+    {
+        Animal[] animals = new Animal[2];
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
+        foreach (Animal animal in animals)
+        {
+            Console.WriteLine(animal.MakeSound());
+            if (animal is Chicken)
+            {
+                Edible edibler = (Chicken)animal;
+                Console.WriteLine(edibler.HowToEat());
+            }
+        }
+
+        Console.WriteLine();
+
+        Fruit[] fruits = new Fruit[2];
+        fruits[0] = new Orange();
+        fruits[1] = new Apple();
+        foreach (Fruit fruit in fruits)
+        {
+            Console.WriteLine(fruit.HowToEat());
+        }
+    }
+}
