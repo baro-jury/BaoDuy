c164d6a116d0e28552f6302f599c4fab81c05e9b
BaoDuy <duypl572002@gmail.com>
2023-09-12 22:28:07 +0700

    Update assignment 13/09/2023

---
+++ Ass10_Sep13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            List<char> characters = new List<char>();
+            characters.InsertRange(0, new char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+            int value = characters[6];
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
