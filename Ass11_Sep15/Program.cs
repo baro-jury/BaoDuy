cb8f7799378be744b86da76dbc596ef1e0522a4b
duylb <duylbhe163097@fpt.edu.vn>
2023-10-06 01:39:32 +0700

    Update assignment 15/09/2023

--- Ass11_Sep15/Program.cs
+++ Ass11_Sep15/Program.cs
@@ -1,12 +1,13 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        SumOfNumbersInTextFile();
+        //SumOfNumbersInTextFile();
         Console.WriteLine("-------------------------------");
-        //CreateComparableCircle();
+        CopyLargeFiles();
         Console.WriteLine("-------------------------------");
         //CreateCircleComparer();
         Console.WriteLine("-------------------------------");
@@ -24,6 +25,57 @@ public class Program
         Console.WriteLine("File path: " + path);
         ReadTextFileExample example = new ReadTextFileExample();
         example.ReadTextFile(path);
+    }
+
+    public static void CopyLargeFiles()
+    {
+        Console.WriteLine("Enter source file: ");
+        string sourcePath = Console.ReadLine();
+        Console.WriteLine("Enter destination file: ");
+        string destinationPath = Console.ReadLine();
+
+        FileInfo source = new FileInfo(sourcePath);
+        FileInfo des = new FileInfo(destinationPath);
+        try
+        {
+            //CopyFileUsingFileInfo(source, des);
+            CopyFileUsingStream(source, des);
+            Console.WriteLine("Copy Completed");
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Cannot Copy");
+            Console.Error.WriteLine(e.Message);
+        }
+    }
+    private static void CopyFileUsingFileInfo(FileInfo source, FileInfo des)
+    {
+        Console.WriteLine("Start Copy using FileInfo");
+        source.CopyTo(des.FullName, true);
+    }
+    private static void CopyFileUsingStream(FileInfo source, FileInfo des)
+    {
+        Console.WriteLine("Start Copy using Stream");
+        StreamReader reader = null;
+        StreamWriter writer = null;
+        try
+        {
+            reader = new StreamReader(source.FullName);
+            writer = new StreamWriter(des.FullName);
+            char[] buffer = new char[1024];
+            int length;
+            while ((length = reader.Read(buffer)) > 0)
+            {
+                writer.Write(buffer, 0, length);
+            }
+        }
+        finally
+        {
+            reader.Close();
+            reader.Dispose();
+            writer.Close();
+            writer.Dispose();
+        }
     }
 }
 
